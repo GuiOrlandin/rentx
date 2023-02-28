91ce44a6d7a43c5bacda058b37aba059b7bf5cb1
Guilherme Orlandin <guiorlandin@gmail.com>
2023-02-28 00:23:27 -0300

    csv parse implement

---
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -0,0 +1,16 @@
+import fs from "fs";
+import csvParse from "csv-parser";
+
+export class ImportCategoryUseCase {
+  execute(file: Express.Multer.File): void {
+    const stream = fs.createReadStream(file.path);
+
+    const parseFile = csvParse();
+
+    stream.pipe(parseFile);
+
+    parseFile.on("data", async (line) => {
+      console.log(line);
+    });
+  }
+}
